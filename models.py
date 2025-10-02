a3cf3613e5281e3e7d0745dd29d00a8befdddcdc
LVR <venkataramanareddylachammagari@gmail.com>
2025-10-02 13:06:01 +0530

    config and model codes are added

---
+++ models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    stats = db.relationship('Stat', backref='user', lazy=True)
+
+class Stat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    wpm = db.Column(db.Float)
+    accuracy = db.Column(db.Float)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
